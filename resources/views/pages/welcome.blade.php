bf4631f41caf75240b52ee7eae9d894ad1ff24f9
MichelPierrel <michel.pierrel@viacesi.fr>
2021-03-22 11:03:30 +0100

    2e changement titre

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -6,6 +6,6 @@ Hello city
 
 @section('content')
             <h1>la cité de nancy</h1>
-            <p>Il est {{date('h:i A')}}</p>
+            <p>Il est {{date('h:i A')}}, trop bien</p>
             
  @endsection
