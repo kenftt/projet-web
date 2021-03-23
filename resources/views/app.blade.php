1adf657560db367e5e67d11b7814aadb1281a32a
Ken Fetet <ken.fetet@viacesi.fr>
2021-03-23 12:35:00 +0100

    Ajout navbar et carte

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,15 +7,18 @@
         <title>@yield('title',config('app.name'))</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+        {{-- <link rel="stylesheet" href="resources/css/app.css"> --}}
+
 
     </head>
     <body>
+        @include('pages.nav')
         <main role="main">
             @yield('content')
         </main>
 
 
-        <footer>
+        <footer class="footer">
             <p class="text-muted">&copy; Copyright {{date('Y')}}
             @if (!Route::is('about'))
                 &middot; <a href="{{ route('about') }}">About us</p>
---
+++ resources/views/pages/nav.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg  navbar-light" style="background-color: #648AD4;">
+    <div class="container-fluid">
+      <a class="navbar-brand text-white" href="#">CESI Stage</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="home">Accueil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Offre</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Entreprise</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Profil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Wishlist</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
