ee8917a35ea8e1c816114c493ef8811e4675bc24
Ken Fetet <ken.fetet@viacesi.fr>
2021-03-25 16:34:55 +0100

    FONCTIONNEL

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,10 +15,7 @@
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
     <!-- Favicons -->
     <link rel="icon" href="{{asset('img/favicon.ico')}}">
-    <!-- Test -->
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
 
 </head>
 
