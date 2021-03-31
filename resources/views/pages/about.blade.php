c17b7cd8ac9f177e9e95795bc103133597b74422
Ken Fetet <ken.fetet@viacesi.fr>
2021-03-31 14:37:54 +0200

    about us modifier

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -42,16 +42,15 @@
     <div class="flex flex-wrap -mx-3 mb-6">
       <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-          First Name
+          Prénom
         </label>
-        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane">
-        <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Prénom">
       </div>
       <div class="w-full md:w-1/2 px-3">
         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-          Last Name
+          Nom
         </label>
-        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe">
+        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Nom">
       </div>
     </div>
     <div class="flex flex-wrap -mx-3 mb-6">
@@ -60,7 +59,6 @@
           E-mail
         </label>
         <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email">
-        <p class="text-gray-600 text-xs italic">Some tips - as long as needed</p>
       </div>
     </div>
     <div class="flex flex-wrap -mx-3 mb-6">
@@ -79,11 +77,11 @@
   </form>
   <div class="button md:w-1/3">
     <button class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-      pierr est une pute
+      Envoyer un mail
     </button>
   </div>
     <div class=revenir>
-    <p><a href="{{ route ('offre')}}">Revenir à la page d'accueil</a></p>
+    <p><a href="offers-">Revenir à la page d'accueil</a></p>
 </div>
 
 <p class="pt-6 flex items-center">
